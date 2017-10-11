3972f832cdb8f9ef48fdadaf4562add0c950dfb9
Timothy Addison <git@tjaddison.com>
2017-10-11 20:41:09 +0100

    Remove show failed only

--- examples/SingleCheck/RunChecks.ps1
+++ examples/SingleCheck/RunChecks.ps1
@@ -3,4 +3,4 @@
 [string]$data = Get-Content -Path .\localhost.config.json -Raw
 $data | ConvertFrom-Json -OutVariable configs
 
-Invoke-Pester -Script @{Path='..\..\tests';Parameters= @{configs=$configs}} -show Failed
+Invoke-Pester -Script @{Path='..\..\tests';Parameters= @{configs=$configs}}
