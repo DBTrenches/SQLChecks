c207a14a43419bbf222a79ac791e3edeff89ff6d
Timothy Addison <git@tjaddison.com>
2017-10-11 20:31:20 +0100

    Move tests to a single file

--- examples/SingleCheck/RunChecks.ps1
+++ examples/SingleCheck/RunChecks.ps1
@@ -1,6 +1,6 @@
 #Requires -Modules DBATools, SQLChecks
 
 [string]$data = Get-Content -Path .\localhost.config.json -Raw
-$data | ConvertFrom-Json -OutVariable cfg
+$data | ConvertFrom-Json -OutVariable configs
 
-Invoke-Pester -Script @{Path='.';Parameters= @{configs=@($cfg)}} 
+Invoke-Pester -Script @{Path='..\..\tests';Parameters= @{configs=$configs}}
