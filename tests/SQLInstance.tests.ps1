ee46b3539f9bbd1f5c081e22bca05e1a09c82676
Timothy Addison <git@tjaddison.com>
2017-10-16 09:02:00 +0100

    Set tests inconclusive in absence of a config value

--- tests/SQLInstance.tests.ps1
+++ tests/SQLInstance.tests.ps1
@@ -6,13 +6,22 @@ Describe "SQL Server Configuration" {
     Context "Instance level settings" {
         foreach($config in $configs) {
             $serverInstance = $config.ServerInstance
-            $traceFlags = $config.TraceFlags
+            
             It "$serverInstance has the correct global trace flags set" {
+                $traceFlags = $config.TraceFlags
+                if($traceFlags -eq $null) {
+                    Set-TestInconclusive -Message "No config value found"
+                }
+
                 (Test-TraceFlags -ServerInstance $serverInstance -ExpectedFlags $traceFlags).Count | Should Be 0
             }
 
-            $maxdop = $config.InstanceMaxDop
             It "$serverInstance has the correct MAXDOP set" {
+                $maxdop = $config.InstanceMaxDop
+                if($maxdop -eq $null) {
+                    Set-TestInconclusive -Message "No config value found"
+                }
+            
                 (Test-InstanceMaxDop -ServerInstance $serverInstance -ExpectedValue $maxdop).Count | Should Be 0
             }
         }
