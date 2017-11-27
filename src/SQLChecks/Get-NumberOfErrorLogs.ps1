d0f5396f955636293faedd7d9087777636c5811c
Timothy Addison <git@tjaddison.com>
2017-11-27 21:15:37 +0000

    Simplify Get-NumberOfErrorLogs

--- src/SQLChecks/Get-NumberOfErrorLogs.ps1
+++ src/SQLChecks/Get-NumberOfErrorLogs.ps1
@@ -4,17 +4,9 @@ Function Get-NumberOfErrorLogs {
         [string] $ServerInstance
     )
 
-    $query = "use master
-    declare @HkeyLocal nvarchar(18)
-    declare @MSSqlServerRegPath nvarchar(31)
-    declare @InstanceRegPath sysname
-    select @HkeyLocal=N'HKEY_LOCAL_MACHINE'
-    select @MSSqlServerRegPath=N'SOFTWARE\Microsoft\MSSQLServer'
-    select @InstanceRegPath=@MSSqlServerRegPath + N'\MSSQLServer'
-    declare @NumErrorLogs int
-    exec master.dbo.xp_instance_regread @HkeyLocal, @InstanceRegPath, N'NumErrorLogs', @NumErrorLogs OUTPUT
-    SELECT
-    ISNULL(@NumErrorLogs, -1) AS [NumberOfErrorLogs];"
+    $query = "declare @NumErrorLogs int;
+    exec master.dbo.xp_instance_regread N'HKEY_LOCAL_MACHINE', N'SOFTWARE\Microsoft\MSSQLServer\MSSQLServer', N'NumErrorLogs', @NumErrorLogs OUTPUT;
+    select isnull(@NumErrorLogs, -1) AS [NumberOfErrorLogs];"
 
     return (Invoke-Sqlcmd -ServerInstance $serverInstance -query $query).NumberOfErrorLogs 
 }
