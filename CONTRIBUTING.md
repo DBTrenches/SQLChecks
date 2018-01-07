8a969ee3d4388671b6acfcec3ddaa64c633080a7
Timothy Addison <git@tjaddison.com>
2018-01-07 11:46:12 +0000

    Split tests to two files, add CONTRIBUTING

---
+++ CONTRIBUTING.md
@@ -0,0 +1,7 @@
+# SQLChecks Contribution Guidelines
+- One file per PowerShell function
+- Update at least one example config with each new test
+- Add documentation for each new test
+- Verb selection: Test if logic is being applied, Get if values from the target are being returned
+- Locate tests in the appropriate test file (instance, database, etc.)
+- Every test must be configurable (either what it tests, or a global on/off option)
--- tests/Databases.tests.ps1
+++ tests/Databases.tests.ps1
@@ -2,26 +2,29 @@ Param(
     $configs
 )
 
-Describe "SQL Server Instance" {
-    Context "Instance level settings" {
+Describe "SQL Server Databases" {
+    Context "Per-Database settings" {
         foreach($config in $configs) {
             $serverInstance = $config.ServerInstance
             
-            It "$serverInstance has the correct global trace flags set" {
-                $traceFlags = $config.TraceFlags
-                if($traceFlags -eq $null) {
+            It "$serverInstance has the all TLogs complying Max Auto Growth" {
+                $MaxTLogAutoGrowthInKB = $config.MaxTLogAutoGrowthInKB
+                if($MaxTLogAutoGrowthInKB  -eq $null) {
                     Set-TestInconclusive -Message "No config value found"
                 }
-
-                (Test-TraceFlags -ServerInstance $serverInstance -ExpectedFlags $traceFlags).Count | Should Be 0
+                (Test-TLogGrowthSize -ServerInstance $serverInstance -MaxTLogAutoGrowthInKB $MaxTLogAutoGrowthInKB).Count | Should Be 0
             }
             
-            It "$serverInstance has the correct number of error logs" {
-                $numErrorLogs = $config.NumErrorLogs
-                if($numErrorLogs  -eq $null) {
+            It "$serverInstance has all the required DDL triggers" {
+                $MustHaveDDLTrigger = $config.MustHaveDDLTrigger
+                if($MustHaveDDLTrigger  -eq $null) {
                     Set-TestInconclusive -Message "No config value found"
                 }
-                Get-NumberOfErrorLogs -ServerInstance $serverInstance | Should Be $numErrorLogs
+                (Test-MustHaveDDLTrigger -ServerInstance $serverInstance -MustHaveDDLTrigger $MustHaveDDLTrigger).Count | Should Be 0
+            }
+
+            It "$serverInstance has no oversized indexes" {
+                (Test-OversizedIndexes -ServerInstance $serverInstance).Count | Should Be 0
             }
         }
     }
