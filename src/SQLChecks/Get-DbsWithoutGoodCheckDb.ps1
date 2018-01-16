b5b8adf98cb2f1b8e4f9c70b66aef7c1ce45b1b3
petervandivier <petervandivier@gmail.com>
2018-01-16 10:13:15 +0000

    did it the easy way... :persevere:

--- src/SQLChecks/Get-DbsWithoutGoodCheckDb.ps1
+++ src/SQLChecks/Get-DbsWithoutGoodCheckDb.ps1
@@ -3,31 +3,9 @@
          [parameter(Mandatory=$true)][int]$MaxDaysAllowedSinceLastGoodCheckDb
         ,[parameter(Mandatory=$true)][string]$ServerInstance
     )
-# ripped shamelessly from https://www.mssqltips.com/sqlservertip/2948/check-dbcc-checkdb-last-execution-using-powershell/
     
-    $lastKnownGood=@()
-
-    $dbs=Get-SqlDatabase -ServerInstance $ServerInstance
-    foreach($db in $dbs){
-        $dbName=$db.Name
-        $dbInfo=(Invoke-Sqlcmd -ServerInstance $ServerInstance -Database $dbName -Query "dbcc dbinfo() with tableresults;")
-        $dbInfo | Add-Member -Type NoteProperty -Name dbName -Value $dbName
-        $lastKnownGood+=($dbInfo | Where-Object {$_.Field -eq "dbi_dbccLastKnownGood"} | Select-Object dbName, Field, Value)
-    }
-
-    $lastKnownGood | Add-Member -Type NoteProperty -Name DaysSinceLastGoodCheckDb -Value $null
-
-    foreach($db in $lastKnownGood){
-        [datetime]$dt=$db.VALUE
-        if($dt -eq [datetime]"1900-01-01"){
-        }else{
-            $daysSince=((Get-Date)-$dt)
-            $db.DaysSinceLastGoodCheckDb=$daysSince.Days
-        }
-    }
-
-    $lastKnownGood | Where-Object {
+    (Get-DbaLastGoodCheckDb -SqlServer $ServerInstance)| Where-Object {
         ($_.DaysSinceLastGoodCheckDb -gt $MaxDaysAllowedSinceLastGoodCheckDb) `
-        -or ($_.DaysSinceLastGoodCheckDb -eq $null)
-    } | Select dbName,DaysSinceLastGoodCheckDb
+        -or ($_.LastGoodCheckDb -eq $null)
+    } | Select Database,LastGoodCheckDb,DaysSinceLastGoodCheckDb
 }
