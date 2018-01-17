f018dc5241d097efb07bafc380750c70510b5d13
petervandivier <petervandivier@gmail.com>
2018-01-17 09:22:54 +0000

    exclude tempdb & handle for additional exclusions

--- src/SQLChecks/Get-DbsWithoutGoodCheckDb.ps1
+++ src/SQLChecks/Get-DbsWithoutGoodCheckDb.ps1
@@ -2,9 +2,14 @@
     [cmdletbinding()]Param(
          [parameter(Mandatory=$true)][int]$MaxDaysAllowedSinceLastGoodCheckDb
         ,[parameter(Mandatory=$true)][string]$ServerInstance
+        ,$excludeDbs # optional array or comma-delim string
     )
     
-    (Get-DbaLastGoodCheckDb -SqlServer $ServerInstance)| Where-Object {
+    $excludeDb=@()
+    $excludeDb+="tempdb" # always exclude
+    if($excludeDbs -ne $null){$excludeDb+=$excludeDbs.Split(",")}
+
+    (Get-DbaLastGoodCheckDb -SqlServer $ServerInstance -ExcludeDatabase $excludeDb)| Where-Object {
         ($_.DaysSinceLastGoodCheckDb -gt $MaxDaysAllowedSinceLastGoodCheckDb) `
         -or ($_.LastGoodCheckDb -eq $null)
     } | Select Database,LastGoodCheckDb,DaysSinceLastGoodCheckDb
