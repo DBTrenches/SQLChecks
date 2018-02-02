d63fe1f63ea8c551b9d2f910a39ff701e0a39e7f
Timothy Addison <git@tjaddison.com>
2018-02-02 21:41:33 +0000

    Embed sql scripts

--- src/SQLChecks/Get-FixedSizeFileGroups.ps1
+++ src/SQLChecks/Get-FixedSizeFileGroups.ps1
@@ -7,7 +7,22 @@
     $WLFGNames=@()
     if($WhitelistFilegroups -ne $null){$WLFGNames+=$WhitelistFilegroups.Split(",")}
 
-    $query=(gc $PSScriptRoot/../SQLScripts/auto-growth-zero.sql -Raw)
+    $query=@"
+select database_id
+    ,[file_id]
+    ,[type]
+    ,[type_desc]
+    ,[state]
+    ,state_desc
+    ,growth
+    ,size_mb=try_convert(float,(try_cast(size as bigint)*8192.)/power(1024,2)) 
+    ,max_size
+    ,[db_name]=db_name(database_id)
+    ,f_name=[name]
+    ,f_path=physical_name
+from sys.master_files 
+where growth = 0;
+"@
 
     (Invoke-Sqlcmd -ServerInstance $ServerInstance -Database master -Query $query) | where {
         $WLFGNames -notcontains $_.f_name
