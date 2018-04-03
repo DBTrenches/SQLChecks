9aed1c5847f311ccce8f6f1e01f0b77ba621cbb5
Timothy Addison <Timothy.Addison@viagogo.com>
2018-04-03 14:49:31 +0100

    Add support for primary only checks

--- src/SQLChecks/Get-DatabasesToCheck.ps1
+++ src/SQLChecks/Get-DatabasesToCheck.ps1
@@ -2,21 +2,30 @@ Function Get-DatabasesToCheck {
     [cmdletbinding()]
     Param(
         [parameter(Mandatory=$true)][string]$ServerInstance
+        ,[switch]$PrimaryOnly
     )
-    # TODO: Allow a switch to specify all/primary only databases
 
     $query = @"
 SELECT  d.name as DatabaseName
-        ,case when rs.is_primary_replica = 1 then 1 else 0 end as IsPrimaryReplica
-        ,case when rs.database_id is not null then 1 else 0 end as IsAvailabilityGroupDatabase
+    ,ag.IsAvailabilityGroupDatabase
+    ,ag.IsPrimaryReplica
 from sys.databases as d
 left join sys.dm_hadr_database_replica_states as rs
 on d.database_id = rs.database_id
+and rs.is_local = 1
+outer apply (
+select  case when rs.database_id is null then 0 else 1 end as IsAvailabilityGroupDatabase
+        ,case when rs.is_primary_replica = 1 then 1 else 0 end as IsPrimaryReplica
+) as ag
 where d.state_desc = 'ONLINE'
-and (rs.database_id is null or (rs.is_local = 1 and rs.is_primary_replica = 1))
 "@
 
-    Invoke-Sqlcmd -ServerInstance $serverInstance -query $query | ForEach-Object {
-        $_.DatabaseName
+    Invoke-Sqlcmd -ServerInstance $serverInstance -query $query | Sort-Object -Property DatabaseName | ForEach-Object {
+        if(-not $PrimaryOnly)
+        {
+            $_.DatabaseName
+        } elseif($_.IsPrimaryReplica -or -not $_.IsAvailabilityGroupDatabase) {
+            $_.DatabaseName
+        }
     }
 }
