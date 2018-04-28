d893946e203e885e13e4ea7aa93e02e47a92bd63
Timothy Addison <git@tjaddison.com>
2018-04-28 18:04:18 +0100

    Remove AG checks from functions

--- src/SQLChecks/Functions/Get-TLogWithPercentageGrowth.ps1
+++ src/SQLChecks/Functions/Get-TLogWithPercentageGrowth.ps1
@@ -12,19 +12,7 @@ select  d.name as DatabaseName
 from    sys.master_files s
 join    sys.databases as d
 on      s.database_id = d.database_id
-where   (
-         d.replica_id is null
-   or    exists
-(
- select  *
- from    sys.availability_databases_cluster as adc
- join    sys.dm_hadr_availability_replica_states as dhars
- on      dhars.group_id = adc.group_id
- where   dhars.role = 1
- and     adc.database_name = d.name
-)
-     )
-and s.type = 1
+where  s.type = 1
 and s.is_percent_growth =1
 and s.database_id = db_id();
 "@
--- src/SQLChecks/Functions/Get-TLogsWithLargeGrowthSize.ps1
+++ src/SQLChecks/Functions/Get-TLogsWithLargeGrowthSize.ps1
@@ -13,19 +13,7 @@ Function Get-TLogsWithLargeGrowthSize {
 from    sys.master_files s
 join    sys.databases as d
 on      s.database_id = d.database_id
-where   (
-         d.replica_id is null
-   or    exists
-(
- select  *
- from    sys.availability_databases_cluster as adc
- join    sys.dm_hadr_availability_replica_states as dhars
- on      dhars.group_id = adc.group_id
- where   dhars.role = 1
- and     adc.database_name = d.name
-)
-     )
-and     s.type = 1
+where   s.type = 1
 and (( s.growth * 8 ) > $GrowthSizeKB and s.is_percent_growth = 0)
 and s.database_id = db_id();
 "@
