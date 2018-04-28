d1c4e5d82390060930f53db8db4ffc66064d3247
Timothy Addison <git@tjaddison.com>
2018-04-28 18:11:40 +0100

    Add support for AG in databases to check

--- src/SQLChecks/Functions/Get-DatabasesToCheck.ps1
+++ src/SQLChecks/Functions/Get-DatabasesToCheck.ps1
@@ -1,22 +1,26 @@
 Function Get-DatabasesToCheck {
     [cmdletbinding()]
     Param(
-        [parameter(Mandatory=$true)][string]$ServerInstance
-        ,[string[]]$ExcludedDatabases
-        ,[switch]$IncludeSecondary = $false
-        ,[switch]$ExcludeSystemDatabases
-        ,[switch]$ExcludePrimary = $false
-        ,[switch]$ExcludeLocal = $false
+        [parameter(Mandatory=$true)][string] $ServerInstance
+        ,[string[]] $ExcludedDatabases
+        ,[switch] $IncludeSecondary = $false
+        ,[switch] $ExcludeSystemDatabases
+        ,[switch] $ExcludePrimary = $false
+        ,[switch] $ExcludeLocal = $false
+        ,[string] $AvailabilityGroup
     )
 
     $query = @"
-SELECT  d.name as DatabaseName
+select  d.name as DatabaseName
     ,ag.IsAvailabilityGroupDatabase
     ,ag.IsPrimaryReplica
+	,grp.name as AvailabilityGroup
 from sys.databases as d
 left join sys.dm_hadr_database_replica_states as rs
 on d.database_id = rs.database_id
 and rs.is_local = 1
+left join sys.availability_groups as grp
+on grp.group_id = rs.group_id
 outer apply (
 select  case when rs.database_id is null then 0 else 1 end as IsAvailabilityGroupDatabase
         ,case when rs.is_primary_replica = 1 then 1 else 0 end as IsPrimaryReplica
@@ -33,6 +37,11 @@ where d.state_desc = 'ONLINE'
 
     Invoke-Sqlcmd -ServerInstance $serverInstance -query $query | Sort-Object -Property DatabaseName | ForEach-Object {       
         if($ExcludedDatabases -contains $_.DatabaseName) {
+            return
+        }
+
+        # If an AG is specified only process databases in this AG
+        if($AvailabilityGroup -ne "" -and $_.AvailabilityGroup -ne $AvailabilityGroup) {
             return
         }
 
