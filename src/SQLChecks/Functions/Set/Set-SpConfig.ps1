73aa8e2ddd771694d93c4bfb1208ae4781a2981d
Tim Addison <git@tjaddison.com>
2018-05-31 21:44:08 +0100

    Add extra verbose output

--- src/SQLChecks/Functions/Set/Set-SpConfig.ps1
+++ src/SQLChecks/Functions/Set/Set-SpConfig.ps1
@@ -7,14 +7,20 @@ Function Set-SpConfig {
     )
     $serverInstance = $Config.ServerInstance
 
+    $total = 0
+    $changed = 0
     foreach($configProperty in $spconfig.PSObject.Properties) {
+        $total++
         $configName = $configProperty.Name
         $expectedValue = $configProperty.Value
         $configValue = (Get-DbaSpConfigure -Server $serverInstance -ConfigName $configName).ConfiguredValue
         
         if($expectedValue -ne $configValue) {
+            $changed++
             Write-Verbose "Updating $configName on $serverInstance from $configValue to $expectedValue"
             Set-DbaSpConfigure -Server $serverInstance -Name $configName -Value $expectedValue
         }
     }
+
+    Write-Verbose "sp_configure update on $serverInstance complete - $changed/$total values updated"
 }
