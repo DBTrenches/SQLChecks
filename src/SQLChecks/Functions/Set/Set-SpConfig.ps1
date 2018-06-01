8da2c137f92bf76d16cccdc63fb381f0bf4b4d92
Timothy Addison <Timothy.Addison@viagogo.com>
2018-06-01 13:38:55 +0100

    Remove string from Config param

--- src/SQLChecks/Functions/Set/Set-SpConfig.ps1
+++ src/SQLChecks/Functions/Set/Set-SpConfig.ps1
@@ -2,19 +2,24 @@ Function Set-SpConfig {
     [cmdletbinding()]
     Param(
         [Parameter(Mandatory=$true)]
-        [string]
         $Config
     )
     $serverInstance = $Config.ServerInstance
 
-    foreach($configProperty in $spconfig.PSObject.Properties) {
+    $total = 0
+    $changed = 0
+    foreach($configProperty in $Config.SpConfig.PSObject.Properties) {
+        $total++
         $configName = $configProperty.Name
         $expectedValue = $configProperty.Value
         $configValue = (Get-DbaSpConfigure -Server $serverInstance -ConfigName $configName).ConfiguredValue
         
         if($expectedValue -ne $configValue) {
+            $changed++
             Write-Verbose "Updating $configName on $serverInstance from $configValue to $expectedValue"
-            Set-DbaSpConfigure -Server $serverInstance -Name $configName -Value $expectedValue
+            Set-DbaSpConfigure -Server $serverInstance -ConfigName $configName -Value $expectedValue | Out-Null
         }
     }
+
+    Write-Verbose "Completed updates for $serverInstance. $changed/$total settings updated"
 }
