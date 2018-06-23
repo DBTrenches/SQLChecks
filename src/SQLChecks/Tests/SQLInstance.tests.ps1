e2e0f9f93d255e40a98c06ddfc2da7d28313023a
Tim Addison <git@tjaddison.com>
2018-06-23 16:08:49 +0000

    Remove set-testinconclusive from error logs, trace flags

--- src/SQLChecks/Tests/SQLInstance.tests.ps1
+++ src/SQLChecks/Tests/SQLInstance.tests.ps1
@@ -8,9 +8,6 @@ Describe "Trace flags" -Tag TraceFlags {
     Context "Testing for traceflags on $serverInstance" {
         It "Correct global trace flags set on $serverInstance" {
             $traceFlags = $config.TraceFlags
-            if($traceFlags -eq $null) {
-                Set-TestInconclusive -Message "No config value found"
-            }
 
             @(Test-TraceFlags -ServerInstance $serverInstance -ExpectedFlags $traceFlags).Count | Should Be 0
         }
@@ -21,9 +18,7 @@ Describe "Number of SQL error logs" -Tag NumErrorLogs {
     Context "Testing for number of SQL error logs on $serverInstance" {
         It "Correct number of SQL error logs on $serverInstance" {
             $numErrorLogs = $config.NumErrorLogs
-            if($numErrorLogs  -eq $null) {
-                Set-TestInconclusive -Message "No config value found"
-            }
+
             (Get-NumberOfErrorLogs -ServerInstance $serverInstance).NumberOfErrorLogs | Should Be $numErrorLogs
         }
     }
