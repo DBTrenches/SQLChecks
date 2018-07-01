26f37ed5140c9bbce72fa10b52d819d12a556ff0
Tim Addison <git@tjaddison.com>
2018-07-01 10:42:48 +0100

    Check module manifest is valid

---
+++ tests/SQLChecks.Manifest.tests.ps1
@@ -0,0 +1,10 @@
+$manifestLocation = "$PSScriptRoot\..\src\SQLChecks\SQLChecks.psd1"
+
+Describe "SQLChecks Manifest" {  $manifest = $null
+
+  It "loads correctly" {
+    {
+      $Script:manifest = Test-ModuleManifest -Path $manifestLocation -ErrorAction Stop 
+    } | Should Not Throw
+  }
+}
