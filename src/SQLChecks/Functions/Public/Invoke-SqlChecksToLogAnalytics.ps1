2c6471d269d0f156d93afa71df05b633267b2894
Tim Addison <git@tjaddison.com>
2018-09-12 21:19:43 -0400

    Add wrapped Invoke-SqlChecks function for exporting to LogAnalytics

---
+++ src/SQLChecks/Functions/Public/Invoke-SqlChecksToLogAnalytics.ps1
@@ -0,0 +1,53 @@
+Function Invoke-SqlChecksToLogAnalytics {
+    [cmdletbinding()]
+    Param(
+        [Parameter(Mandatory=$true)]
+        $Config,
+
+        [Parameter(Mandatory=$true)]
+        $BatchId,
+
+        [Parameter(Mandatory=$true)]
+        $CustomerId,
+
+        [Parameter(Mandatory=$true)]
+        $SharedKey
+    )
+
+    $invocationStartTime = [DateTime]::UtcNow
+    $results = Invoke-SqlChecks -Config $config -PassThru -Show None
+    $invocationEndTime = [DateTime]::UtcNow
+
+    if($Results.Count -gt 0) {
+        $pesterResults = @()
+        foreach($testResult in $results.TestResult) {
+            $pesterResults += [PSCustomObject]@{
+                BatchId = $BatchId
+                InvocationId = [System.Guid]::NewGuid()
+                InvocationStartTime = $InvocationStartTime
+                InvocationEndTime = $InvocationEndTime
+                HostComputer = $env:computername
+                TargetComputer = $config.ServerInstance
+                TimeTaken = $testResult.Time.TotalMilliseconds
+                Passed = $testResult.Passed
+                Describe = $testResult.Describe
+                Context = $testResult.Context
+                Name = $testResult.Name
+                FailureMessage = $testResult.FailureMessage
+                Result = $testResult.Result
+            }
+        }
+
+        $exportArguments = @{
+            CustomerId = $CustomerId
+            SharedKey = $SharedKey
+            LogType = "PesterResult"
+            TimeStampField = "InvocationStartTime"
+        }
+
+        Write-Verbose "Exporting $($pesterResults.Count) results"
+        Export-LogAnalytics @exportArguments $pesterResults
+    } else {
+        Write-Verbose "No test results for $($config.ServerInstance)"
+    }
+}
