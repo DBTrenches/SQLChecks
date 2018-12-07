9808e3e90f9c637f33ccd8a6681d152ac232c303
Tim Addison <git@tjaddison.com>
2018-12-06 20:53:47 -0500

    Format test files

--- src/SQLChecks/Tests/SQLAgent.tests.ps1
+++ src/SQLChecks/Tests/SQLAgent.tests.ps1
@@ -1,23 +1,23 @@
 Param(
-    $Config
+  $Config
 )
 
 $serverInstance = $config.ServerInstance
 
 Describe "SQL Agent job schedules" -Tag AgentJobOneActiveSchedule {
-    It "No jobs without at least one active schedule on $serverInstance" {
-        @(Get-SqlAgentJobsWithNoActiveSchedule -Config $Config).Count | Should Be 0
-    }
+  It "No jobs without at least one active schedule on $serverInstance" {
+    @(Get-SqlAgentJobsWithNoActiveSchedule -Config $Config).Count | Should Be 0
+  }
 }
 
 Describe "SQL Agent schedules" -Tag AgentJobNoDisabledSchedules {
-    It "No jobs with any disabled schedules on $serverInstance" {
-        @(Get-SqlAgentJobsWithDisabledSchedule -Config $Config).Count | Should Be 0
-    }
+  It "No jobs with any disabled schedules on $serverInstance" {
+    @(Get-SqlAgentJobsWithDisabledSchedule -Config $Config).Count | Should Be 0
+  }
 }
 
 Describe "SQL Agent status" -Tag AgentIsRunning {
-    It "SQL Agent is running on $serverInstance" {
-        (Get-SqlAgentService -Config $Config).Status | Should Be "Running"
-    }
+  It "SQL Agent is running on $serverInstance" {
+    (Get-SqlAgentService -Config $Config).Status | Should Be "Running"
+  }
 }
--- src/SQLChecks/Tests/SQLInstance.tests.ps1
+++ src/SQLChecks/Tests/SQLInstance.tests.ps1
@@ -1,48 +1,48 @@
 Param(
-    $Config
+  $Config
 )
 
 $serverInstance = $config.ServerInstance
 
 Describe "Trace flags" -Tag TraceFlags {
-    It "Correct global trace flags set on $serverInstance" {
-        @(Test-TraceFlags $Config).Count | Should Be 0
-    }
+  It "Correct global trace flags set on $serverInstance" {
+    @(Test-TraceFlags $Config).Count | Should Be 0
+  }
 }
 
 Describe "Number of SQL error logs" -Tag NumErrorLogs {
-    It "Correct number of SQL error logs on $serverInstance" {
-        $numErrorLogs = $Config.NumErrorLogs
+  It "Correct number of SQL error logs on $serverInstance" {
+    $numErrorLogs = $Config.NumErrorLogs
 
-        (Get-NumberOfErrorLogs -Config $Config).NumberOfErrorLogs | Should Be $numErrorLogs
-    }
+    (Get-NumberOfErrorLogs -Config $Config).NumberOfErrorLogs | Should Be $numErrorLogs
+  }
 }
 
 Describe "SPConfigure values" -Tag SpConfig {
-    $spconfig = $Config.SpConfig
+  $spconfig = $Config.SpConfig
 
-    foreach($configProperty in $spconfig.PSObject.Properties) {
-        $configName = $configProperty.Name
-        $expectedValue = $configProperty.Value
+  foreach ($configProperty in $spconfig.PSObject.Properties) {
+    $configName = $configProperty.Name
+    $expectedValue = $configProperty.Value
 
-        It "Correct sp_configure setting $configName on $serverInstance " {
-            (Get-SpConfigValue -Config $Config -ConfigName $configName).ConfiguredValue | Should Be $expectedValue
-        }
+    It "Correct sp_configure setting $configName on $serverInstance " {
+      (Get-SpConfigValue -Config $Config -ConfigName $configName).ConfiguredValue | Should Be $expectedValue
     }
+  }
 }
 
 Describe "Startup Extended Events" -Tag StartupXEvents {
-    It "has correct set of startup extended event sessions on $serverInstance" {
-        @(Test-StartupXEvents -Config $Config).Count | Should Be 0
-    }
+  It "has correct set of startup extended event sessions on $serverInstance" {
+    @(Test-StartupXEvents -Config $Config).Count | Should Be 0
+  }
 }
 
 Describe "Database Mail is configured" -Tag DatabaseMail {
-    It "Database mail is enabled on $serverInstance" {
-        (Get-SpConfigValue -Server $serverInstance -ConfigName "Database Mail XPs").ConfiguredValue | Should Be 1
-    }
+  It "Database mail is enabled on $serverInstance" {
+    (Get-SpConfigValue -Server $serverInstance -ConfigName "Database Mail XPs").ConfiguredValue | Should Be 1
+  }
 
-    It "Database mail has a default profile configured on $serverInstance" {
-        @(Get-DefaultDatabaseMailProfile -Config $Config).Count | Should Be 1
-    }
+  It "Database mail has a default profile configured on $serverInstance" {
+    @(Get-DefaultDatabaseMailProfile -Config $Config).Count | Should Be 1
+  }
 }
