2b437eb08b7bef39b0fd5153ec536549fc770ec0
Tim Addison <git@tjaddison.com>
2018-12-10 20:58:23 -0500

    Add AG test file

---
+++ src/SQLChecks/Tests/AvailabilityGroup.tests.ps1
@@ -0,0 +1,17 @@
+Param(
+  $Config
+)
+
+$serverInstance = $config.ServerInstance
+$availabilityGroup = $config.AvailabilityGroup
+
+Describe "Listener connectivity" -Tag AGInstanceConnectivity {
+  It "Can connect to $serverInstance" {
+    {
+      Invoke-SqlCmd -ServerInstance $serverInstance -Query "select @@servername" -Database master
+    } | Should -Not -Throw
+  }
+}
+
+## TODO
+# Sync secondary count (synchronized) -> all DBs? or foreach DB?
