c806feba9ae019ce5eff32be183b1f4ca9ee093e
Timothy Addison <Timothy.Addison@viagogo.com>
2018-12-12 09:23:29 -0500

    Fix the availability group bugs

--- src/SQLChecks/Tests/AvailabilityGroup.tests.ps1
+++ src/SQLChecks/Tests/AvailabilityGroup.tests.ps1
@@ -14,17 +14,13 @@ Describe "AG Instance connectivity" -Tag AGInstanceConnectivity {
 }
 
 Describe "All synchronous commit secondaries are healthy" -Tag AGSyncCommitHealthStatus {
-  # Results has one row per db-replica
-  # For each primary DB, there should be N healthy sync-commit replicas
-  #$dbSummary = $results | Where-Object { $_.is_primary_replica -eq $true } | Select-Object -ExpandProperty database_name
   $dbSummary = Get-AGDatabaseSummary $config
 
   $replicaCount = $config.AGSyncCommitHealthStatus.NumberOfReplicas
 
   foreach($db in $dbSummary) {
-    It "$db has the $replicaCount healthy sync commit secondaries for $availabilityGroup on $serverInstance" {
-      #@($results | Where-Object { $_.database_name -eq $db -and $_.synchronization_state_desc -eq "SYNCHRONIZED" -and $_.is_primary_replica -eq $false }).Count | Should -Be 1
-      $db.SynchronizedReplicaCount | Should -Be $replicaCount
+    It "$($db.DatabaseName) has $replicaCount healthy sync commit secondaries for $availabilityGroup on $serverInstance" {
+      $db.SynchronizedReplicas | Should -Be $replicaCount
     }
   }
 }
