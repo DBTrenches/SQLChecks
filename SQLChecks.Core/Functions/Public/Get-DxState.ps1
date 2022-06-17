384464c798ebc3d3af50d56b3e14a536cb3bc449
PeterVandivier <peter.vandivier@gmail.com>
2022-06-17 14:10:17 -0400

    exclude SMO metadata from DxState

--- SQLChecks.Core/Functions/Public/Get-DxState.ps1
+++ SQLChecks.Core/Functions/Public/Get-DxState.ps1
@@ -10,7 +10,8 @@ function Get-DxState {
 
     $DxQuery = Get-DxQuery -Tag $Tag
 
-    $DxState = Invoke-DbaQuery -Query $DxQuery -SqlInstance $SqlInstance
+    $DxState = Invoke-DbaQuery -Query $DxQuery -SqlInstance $SqlInstance | 
+        Select-Object * -ExcludeProperty RowError,RowState,Table,ItemArray,HasErrors
 
     if(0 -eq $DxState.Count){
         Write-Verbose "Empty resultset for [$Tag] query against [$SqlInstance]"
