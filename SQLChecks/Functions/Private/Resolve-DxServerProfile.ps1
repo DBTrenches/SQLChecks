957ced77060345c135a282a5fadb255ea58e5f60
PeterVandivier <peter.vandivier@gmail.com>
2022-07-21 14:24:38 -0400

    Resolve-ProfileConfig: group build with set

--- SQLChecks/Functions/Private/Resolve-DxServerProfile.ps1
+++ SQLChecks/Functions/Private/Resolve-DxServerProfile.ps1
@@ -14,45 +14,43 @@ function Resolve-DxServerProfile {
     $ReturnObject = $DxTemplateConfig.Class.Server
 
     # TODO: handle for duplicate/conflicting inputs
-
     $SysAdmins = foreach($node in $InputObject.Security.SysAdmins){
         $DxTemplateConfig.Security.SysAdmins.$node
     }
-
+    $ReturnObject.Security.SysAdmins = $SysAdmins
+    
     $NumErrorLogs = if($DxTemplateConfig.Management.NumErrorLogs){
         $DxTemplateConfig.Management.NumErrorLogs
         # https://docs.microsoft.com/en-us/sql/database-engine/configure-windows/scm-services-configure-sql-server-error-logs
     } else { 6 }
+    $ReturnObject.Management.NumErrorLogs = $NumErrorLogs 
+    
     $Xevents = foreach($node in $InputObject.Management.Xevents){
         $DxTemplateConfig.Management.Xevents.$node
     }
-
+    $ReturnObject.Management.Xevents = $Xevents
+    
     $SysConfigurations = foreach($node in $InputObject.Service.SysConfigurations){
         $DxTemplateConfig.Service.SysConfigurations.$node
     }
+    $ReturnObject.Service.SysConfigurations = $SysConfigurations
+    
     $TempDBConfiguration = $DxTemplateConfig.Service.TempDBConfiguration.$node
+    $ReturnObject.Service.TempDBConfiguration = $TempDBConfiguration
+    
     $TraceFlags = foreach($node in $InputObject.Service.TraceFlags){
         $DxTemplateConfig.Service.TraceFlags.$node
     }
-    
+    $ReturnObject.Service.TraceFlags = $TraceFlags
 
     $Alerts = foreach($node in $InputObject.SqlAgent.Alerts){
         $DxTemplateConfig.SqlAgent.Alerts.$node
     }
+    $ReturnObject.SqlAgent.Alerts = $Alerts
+
     $Operators = foreach($node in $InputObject.SqlAgent.Operators){
         $DxTemplateConfig.SqlAgent.Operators.$node
     }
-
-    $ReturnObject.Security.SysAdmins = $SysAdmins
-
-    $ReturnObject.Management.NumErrorLogs = $NumErrorLogs 
-    $ReturnObject.Management.Xevents = $Xevents
-    
-    $ReturnObject.Service.SysConfigurations = $SysConfigurations
-    $ReturnObject.Service.TempDBConfiguration = $TempDBConfiguration
-    $ReturnObject.Service.TraceFlags = $TraceFlags
-
-    $ReturnObject.SqlAgent.Alerts = $Alerts
     $ReturnObject.SqlAgent.Operators = $Operators
 
     return $ReturnObject
