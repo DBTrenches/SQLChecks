9b80dc69ee4220bb4dc18127fb4396215d3299c4
PeterVandivier <peter.vandivier@gmail.com>
2022-07-27 15:09:31 -0400

    CBH & multi-db improvements to get-state

--- SQLChecks/Functions/Public/Get-DxState.ps1
+++ SQLChecks/Functions/Public/Get-DxState.ps1
@@ -1,4 +1,25 @@
 function Get-DxState {
+<#
+.SYNOPSIS
+    Runs the SqlLibrary query for $Tag against a server and return the current state. 
+
+.DESCRIPTION
+    Every valid $Tag has a corresponding entry at $DxSqlLibrary.$Tag.QueryText. 
+    This Function retrieves the corresponding query and executes it against the
+    given $SqlInstance. Invoke-SqlCmd2 is used because in testing it was found to
+    have far better performance than Invoke-DbaQuery when re-using connections. 
+    Use of the -Database parameter necessarily establishes a new connection object
+    with a different database context for each given 
+
+.PARAMETER Database
+    String array of Database names against which to execute the $Tag'd query. 
+    Uses syntax similar to Ola Hallengren for excluding databases: prefixing a
+    database name with a minus symbol (-) will exlude the database. This is only
+    required when including the star symbol (*) which will include all databases
+    on the server. Fuzzy matching is not supported at this time. If this param
+    includes a database that does not exist on the server, a warning will be raised.
+    If this param is not used, `master` db will be used. 
+#>
     [CmdletBinding()]
     Param(
         [Parameter(Mandatory)]
@@ -17,24 +38,32 @@ function Get-DxState {
     if($PSBoundParameters.Keys -contains 'Database'){
         $AllDatabases = (Invoke-SqlCmd2 -ServerInstance $SqlInstance -Query "select [name] as [Name] from sys.databases").Name
 
-        $NonExistentDatabases = $Database | Where-Object {
+        # NonExistentDatabases 
+        $Database | Where-Object {
             ($_ -NotIn $AllDatabases) -and
-            ($_ -ne "*") 
-        } 
-
-        $NonExistentDatabases | ForEach-Object {
+            ($_ -ne "*") -and
+            ($_[0] -ne '-')
+        } | ForEach-Object {
             Write-Warning "Input database '$_' does not exist on SqlInstance $($SqlInstance.FullName). The entry will be excluded. "
         }
 
-        if($Database -contains "*"){
-            $Database = $AllDatabases
+        if($Database -Contains "*"){
+            $DatabaseCollection = $AllDatabases
         } else {
-            $Database = $Database | Where-Object {$_ -In $AllDatabases}
+            $DatabaseCollection = $Database | Where-Object { $_ -In $AllDatabases }
         }
+
+        [string[]]$ExcludeDatabases = @()
+        $Database | Where-Object { $_.Substring(0,1) -eq '-' } | ForEach-Object {
+            $db = $_.Substring(1)
+            Write-Verbose "Database '$db' will be excluded by user preference. "
+            $ExcludeDatabases += $db
+        }
+        $DatabaseCollection = $DatabaseCollection | Where-Object { $_ -NotIn $ExcludeDatabases }
     }
 
-    $DxState = if($PSBoundParameters.Keys -contains 'Database'){
-        foreach($db in $Database) {
+    $DxState = if($PSBoundParameters.Keys -Contains 'Database'){
+        foreach($db in $DatabaseCollection) {
             Invoke-SqlCmd2 -Query $DxQuery -ServerInstance $SqlInstance -Database $db | 
                 Select-Object *, @{Name="_Database";Expression={$db}}
         }
