a08b7e3be2cd155cdc6cb2d22aa3287c970f9a7c
PeterVandivier <peter.vandivier@gmail.com>
2022-08-15 12:57:20 -0400

    CBH Get-DxTags

--- SqlChecks/Functions/Private/Get-DxTags.ps1
+++ SqlChecks/Functions/Private/Get-DxTags.ps1
@@ -1,4 +1,13 @@
 function Get-DxTags {
+<#
+.SYNOPSIS
+    Only used for PS <= 5.0 compat at this time
+
+.DESCRIPTION
+    See toggleCompat.ps1 in the root of the repo. 
+    This function is only needed when running the 
+    module in PS Desktop compat mode. 
+#>
     '_Utility.select1'
     'Databases.DuplicateIndexes'
     'Databases.Files.SpaceUsed'
