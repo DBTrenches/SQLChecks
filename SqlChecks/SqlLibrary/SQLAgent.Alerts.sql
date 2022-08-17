07164d2d863b2c8f46a27315b059db9d41f9b1cd
PeterVandivier <peter.vandivier@gmail.com>
2022-08-16 22:47:33 -0400

    SqlAgent.Alerts query return more than just name

--- SqlChecks/SqlLibrary/SQLAgent.Alerts.sql
+++ SqlChecks/SqlLibrary/SQLAgent.Alerts.sql
@@ -1,4 +1,10 @@
-select [Name]
+select 
+    [name] as [Name],
+    [enabled] as [Enabled],
+    message_id as MessageId,
+    delay_between_responses as DelayBetweenResponses,
+    notification_message as NotificationMessage,
+    event_description_keyword as EventDescriptionKeyword
 from msdb.dbo.sysalerts
 where [enabled] = 1
   and has_notification = 1;
