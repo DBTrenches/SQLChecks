8215b62e399e332a8c07676dd583e75d4526e6c5
PeterVandivier <peter.vandivier@gmail.com>
2022-08-17 14:05:48 -0400

    Service.tests Init-Check

--- Checks/Service.tests.ps1
+++ Checks/Service.tests.ps1
@@ -1,8 +1,5 @@
 #Requires -Modules @{ModuleName='SqlChecks';ModuleVersion='2.0';Guid='998f41a0-c4b4-4ec5-9e11-cb807d98d969'}
 
-# PsScriptAnalyzer reports false positive for $vars defined in `Discovery` not used until `It`
-[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
-
 [CmdletBinding()]
 Param(
     [string]$EntityName = $DxDefaults.EntityName
@@ -15,53 +12,38 @@ BeforeAll {
     else {
         Write-Verbose "Default entity will be used. "
     }
-
     Write-Host "Selected entity is '$EntityName' "
     Write-Host "The connection string to be used is '$($DxEntityLibrary.$EntityName.ConnectionString)' "
 }
 
-BeforeDiscovery {    
-    $DxEntity = $DxEntityLibrary.$EntityName
-
-    $ConnectionString = $DxEntity.ConnectionString
-
-    $Connect = @{SqlInstance = $ConnectionString}
+AfterAll {
+    Remove-Variable Collection -Force -Scope Global
 }
 
 Describe "Service.TraceFlags " -Tag Service.TraceFlags {
     BeforeDiscovery {
-        $TraceFlagData = @{
-            ServerData = Get-DxState Service.TraceFlags @Connect 
-            ConfigData = $DxEntity.Service.TraceFlags 
-            KeyName = 'TraceFlag'
-        }
-        $TraceFlagCollection = Join-DxConfigAndState @TraceFlagData
+        Initialize-DxCheck Service.TraceFlags -KeyName TraceFlag -EntityName $EntityName
     }
 
-    It "TraceFlag test is running" -ForEach $TraceFlagCollection[0] {
+    It "TraceFlag test is running" -ForEach $Collection[0] {
         ($_.ExistsInConfig).Count | Should -BeExactly 1 -Because "In order to confirm the test ran, an empty object is returned from `Join-DxConfigAndState` when there is a null set on both server and config "
     }
-    Context "TraceFlag: <_.Name> " -ForEach $TraceFlagCollection {
+    Context "TraceFlag: <_.Name> " -ForEach $Collection {
         It "Is in config " {
-            $_.ExistsInConfig | Should -BeTrue -Because "We should have all TFs documented in Config. "
+            $_.ExistsInConfig | Should -BeTrue -Because "We should have all Trace Flags documented in Config. "
         }
         It "Is deployed " {
-            $_.ExistsOnServer | Should -BeTrue -Because "All config TFs should be applied "
+            $_.ExistsOnServer | Should -BeTrue -Because "All config Trace Flags should be applied. "
         }
     }
 }
 
 Describe "Service.SysConfigurations " -Tag Service.SysConfigurations, SpConfigure {
     BeforeDiscovery {
-        $SpConfigData = @{
-            ConfigData = $DxEntity.Service.SysConfigurations 
-            ServerData = Get-DxState Service.SysConfigurations @Connect 
-        }
-
-        $SpConfigCollection = Join-DxConfigAndState @SpConfigData
+        Initialize-DxCheck Service.SysConfigurations -EntityName $EntityName
     }
 
-    It "SysConfiguration: '<_.Name>' " -ForEach $SpConfigCollection {
+    It "SysConfiguration: '<_.Name>' " -ForEach $Collection {
         $_.ExistsInConfig | Should -BeTrue
         $_.ExistsOnServer | Should -BeTrue
         $_.Server.Value | Should -BeExactly $_.Config.Value
@@ -71,21 +53,15 @@ Describe "Service.SysConfigurations " -Tag Service.SysConfigurations, SpConfigur
 
 Describe "Service.TempDbConfiguration " -Tag Service.TempDbConfiguration {
     BeforeDiscovery {
-        $TempDbConfigurationData = @{
-            ConfigData = $DxEntity.Service.TempDbConfiguration 
-            ServerData = Get-DxState Service.TempDbConfiguration @Connect 
-            KeyName = 'DbName'
-        }
-
-        $TempDbConfiguration = Join-DxConfigAndState @TempDbConfigurationData
+        Initialize-DxCheck Service.TempDbConfiguration -KeyName DbName -EntityName $EntityName
     }
 
-    It "NumberOfFiles: '<_.Config.NumberOfFiles>' " -ForEach $TempDbConfiguration {
+    It "NumberOfFiles: '<_.Config.NumberOfFiles>' " -ForEach $Collection {
         $_.ExistsInConfig | Should -BeTrue
         $_.Server.NumberOfFiles | Should -BeExactly $_.Config.NumberOfFiles
     }
     
-    It "TotalSizeMb: '<_.Config.TotalSizeMB>' " -ForEach $TempDbConfiguration {
+    It "TotalSizeMb: '<_.Config.TotalSizeMB>' " -ForEach $Collection {
         $_.ExistsInConfig | Should -BeTrue
         $_.Server.TotalSizeMB | Should -BeExactly $_.Config.TotalSizeMB
     }
@@ -93,12 +69,7 @@ Describe "Service.TempDbConfiguration " -Tag Service.TempDbConfiguration {
 
 Describe "Service.InstantFileInitializationSetting on '$ConnectionString' " -Tag Service.InstantFileInitializationSetting {
     BeforeDiscovery {
-        $Splat = @{
-            ServerData = Get-DxState -Tag Service.InstantFileInitializationSetting @Connect 
-            ConfigData = $DxEntity.Service.InstantFileInitializationSetting 
-            KeyName = "Name"
-        }
-        $Collection = Join-DxConfigAndState @Splat
+        Initialize-DxCheck Service.InstantFileInitializationSetting -EntityName $EntityName
     }
     It "InstantFileInitializationSetting: '<_.Name>' " -ForEach $Collection {
         $_.ExistsOnServer | Should -BeTrue
