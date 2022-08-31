a466a0fa0193d1a746f9082ed4d73ab62fdf2616
PeterVandivier <peter.vandivier@gmail.com>
2022-08-31 12:17:30 -0400

    🎨 sql file formatting

--- SqlChecks/SqlLibrary/Management.ResourceGovernor.ResourceGovernorState.sql
+++ SqlChecks/SqlLibrary/Management.ResourceGovernor.ResourceGovernorState.sql
@@ -1,10 +1,10 @@
-select      concat (
-                object_schema_name (classifier_function_id)
-               ,'.'
-               ,object_name (classifier_function_id)) as ClassifierFunction
-           ,is_enabled as IsEnabled
-           ,cast(dr.is_reconfiguration_pending as bit) as IsReconfigurationPending
-from        sys.resource_governor_configuration
-outer apply (   select  drgc.is_reconfiguration_pending
-                from    sys.dm_resource_governor_configuration as drgc) dr;
-                
+select
+    ClassifierFunction = concat(
+                             object_schema_name(rg.classifier_function_id),
+                             '.',
+                             object_name(rg.classifier_function_id)
+                         ),
+    IsEnabled = rg.is_enabled,
+    IsReconfigurationPending = convert(bit, dr.is_reconfiguration_pending)
+from sys.resource_governor_configuration as rg
+cross join sys.dm_resource_governor_configuration as dr;
